3ec46174328af8e35df6d0e616e90e853fd2d91c
Tejasva Khandelwal <khandelwaltejasva@gmail.com>
2025-03-28 09:34:14 +0530

    set up the basic file structure and theme toggle

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex-center">
+      <div className="box">
+        <h className="heading-primary">Heading</h>
+        <p className="text-primary">Hello world</p>
+      </div>
+    </div>
+  )
+}
+
+export default App;
